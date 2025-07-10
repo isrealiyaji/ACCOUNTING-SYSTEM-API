8d7fedbbd8a3d400ecd55460a1ab044d595caa9e
Isreal Iyaji <isrealiyaji1@gmail.com>
2025-07-10 08:27:33 +0100

    Tried a simple bank project

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 const TaskStatus = {
   PENDING: "pending",
   IN_PROGRESS: "inprogress",
@@ -88,3 +89,126 @@ todo.setTaskStatus(0, TaskStatus.IN_PROGRESS);
 todo.setTaskStatus(1, TaskStatus.COMPLETED);
 
 todo.showTasks;
+*/
+class Transaction {
+  constructor(type, amount) {
+    this.date = new Date();
+    this.type = type;
+    this.amount = amount;
+  }
+  print(acccountType) {
+    return `${this.toLocaleString()} | ${acccountType} | $${this.amount}`;
+  }
+}
+class BankAccount {
+  constructor(accountName, accountNumber, accountType, initialDeposit) {
+    if (
+      typeof accountNumber != "number" ||
+      accountNumber.toString().length !== 10 ||
+      !Number.isInteger(accountNumber)
+    ) {
+      throw new Error(
+        `Invalid account number "${accountNumber}". it must be 10 digits.`
+      );
+    }
+
+    this.accountName = accountName;
+    this.accountNumber = accountNumber;
+    this.accountType = accountType;
+    this.balance = initialDeposit;
+    this.transactions = [];
+
+    this.minimumDeposit = 10;
+    this.minimumWithdrawal = 20;
+    // this.minimumDeposit.
+    // this.minimumwithdrawal.
+
+    this.addTransaction("Account Opened", initialDeposit);
+  }
+
+  addTransaction(type, amount) {
+    const transaction = new Transaction(type, amount);
+    this.transactions.push(transaction);
+  }
+
+  deposit(amount) {
+    if (amount <= this.minimumDeposit) {
+      console.log(
+        `Deposit failed: Deposit should be greater than {this.minimumDeposit}`
+      );
+    }
+    this.balance += amount;
+    this.addTransaction("Deposit", amount);
+    console.log(`$${amount} deposited. New balance: $${this.balance}`);
+  }
+
+  withdraw(amount) {
+    if (amount <= this.minimumWithdrawal) {
+      console.log(
+        `withdrawal failed: Withdrawal amount must be greater{this.minimumWithdrawal}`
+      );
+    }
+    if (amount > this.balance) {
+      console.log("Not enough balance.");
+    }
+    this.balance -= amount;
+    this.addTransaction("withdrawal", amount);
+    console.log(`$${amount} withdrawn. New balance: $${this.balance}`);
+  }
+
+  printTransactions() {
+    console.log(`Transactions for ${this.accountNumber} | ${this.accountName}`);
+    this.transactions.forEach((transaction) => {
+      console.log(
+        `${transaction.date.toLocaleString()} | ${this.accountType} | $${
+          transaction.amount
+        }`
+      );
+    });
+    console.log(`Currernt Balance: $${this.balance}`);
+  }
+}
+
+class User {
+  constructor(name) {
+    this.name = name;
+    this.account = [];
+  }
+
+  addAccount(accountName, accountNumber, accountType, initialDeposit) {
+    const account = new BankAccount(
+      accountName,
+      accountNumber,
+      accountType,
+      initialDeposit
+    );
+    this.account.push(account);
+    console.log(
+      `Added a ${accountType} account with account number ${accountNumber} for ${this.name}`
+    );
+  }
+  getAccount(accountNumber) {
+    return this.account.find(
+      (account) => account.accountNumber === accountNumber
+    );
+  }
+}
+
+const user1 = new User("Jaji");
+
+user1.addAccount("Jaji", 2283176438, "savings", 324500);
+user1.addAccount("Jaji", 8165634041, "current", 432500);
+
+const savings = user1.getAccount(2283176438);
+savings.deposit(700000);
+savings.withdraw(20000);
+savings.printTransactions();
+
+const current = user1.getAccount(8165634041);
+current.withdraw(30000);
+current.printTransactions();
+
+//  How to make sure the account number doesn't take in < or > 11 digit
+// add transaction history table or list
+// add transaction cla
+// The deposit should not be less than 20
